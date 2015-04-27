f8221192bb9867d3991c72cf71f6cd7ce0d2e8c8
kongodream <imlitvinenko@ukr.net>
2015-04-27 22:33:24 +0300

    Modelling db step 1.3 rename type_of_word

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  belongs_to :origin
+  belongs_to :type_of_work
 end
--- db/migrate/20150426213520_create_type_of_works.rb
+++
@@ -1,7 +0,0 @@
-class CreateTypeOfWorks < ActiveRecord::Migration
-  def change
-    create_table :type_of_works do |t|
-      t.string :type
-    end
-  end
-end
